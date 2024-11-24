e72e1021b99a5bdee7dbf1a08de275d2991e7f59
Lee Hon <33125914+Leehon008@users.noreply.github.com>
2024-11-24 03:25:38 +0200

    docs: Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/school-mgt-nextjs",
+  output: "export",
+  reactStrictMode: true,
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
